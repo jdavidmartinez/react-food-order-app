6294f500031809afc51948c353b92636c9212ac3
DaviD D <jdavid.martinez@gmail.com>
2021-06-20 20:09:26 -0500

    Section 11 - Video 134 - Adding 'MEasl' Component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import clasess from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={clasess.summary}>
+      <h2>Delicius food, Deliverede to You</h2>
+      <p>
+        Choose your favorite meal from our briad selection of avalilable meals
+        and enjoy a delicius lunch or dinner at home.
+      </p>
+      <p>
+        All out meals are cooked with high-quality ingredients, just-in-time and
+        of course experienced checf!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
